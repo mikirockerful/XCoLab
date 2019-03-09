c03c28cecdb5513efbc5ecfb45fb3d6dc763333b
miguel <gamallomiguel@gmail.com>
2019-03-09 19:28:20 +0100

    Remove Travis pushes to production servers

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -5,20 +5,22 @@ set -e
 
 if [ ${BUILD_TYPE} == 'deploy' ]; then
 
-    eval "$(ssh-agent -s)"
-    ssh-add deploy_rsa
-    cat scripts/travis/cognosis2.known_host >> $HOME/.ssh/known_hosts
+    #TODO: This should push the built containers to the DockerHub
 
-    WORKER_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+    #eval "$(ssh-agent -s)"
+    #ssh-add deploy_rsa
+    #cat scripts/travis/cognosis2.known_host >> $HOME/.ssh/known_hosts
 
-    DEPLOY_FOLDER=xcolab/${TRAVIS_BRANCH}
-    DEPLOY_SERVER=binaries@cognosis2.mit.edu
-    echo "Copying binaries from ${WORKER_IP} to ${DEPLOY_SERVER}:${DEPLOY_FOLDER} ..."
-    rsync -r --delete-after --quiet binaries ${DEPLOY_SERVER}:${DEPLOY_FOLDER}
+    #WORKER_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
-    # Workaround for hanging builds
-    # taken from https://github.com/travis-ci/travis-ci/issues/8082#issuecomment-315147561
-    ssh-agent -k
+    #DEPLOY_FOLDER=xcolab/${TRAVIS_BRANCH}
+    #DEPLOY_SERVER=binaries@cognosis2.mit.edu
+    #echo "Copying binaries from ${WORKER_IP} to ${DEPLOY_SERVER}:${DEPLOY_FOLDER} ..."
+    #rsync -r --delete-after --quiet binaries ${DEPLOY_SERVER}:${DEPLOY_FOLDER}
+
+    ## Workaround for hanging builds
+    ## taken from https://github.com/travis-ci/travis-ci/issues/8082#issuecomment-315147561
+    #ssh-agent -k
 else
     echo "Skipping deploy step on branch ${TRAVIS_BRANCH}"
 fi
