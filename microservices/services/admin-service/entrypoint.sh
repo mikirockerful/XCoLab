7f33559b7a78dc8dc52fc2063c7e9c932bd4b349
miguel <gamallomiguel@gmail.com>
2019-04-27 18:34:51 +0200

    Add database connection test for compose startup

--- microservices/services/admin-service/entrypoint.sh
+++ microservices/services/admin-service/entrypoint.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
-# Stop execution if any command returns nonzero exit
-set -e
+# Wait for database to be accessible
+database_host=$(grep db\.url\.base /root/.xcolab.application.properties | awk -F '//' '{print $2}' | awk -F ':' '{print $1}')
+database_port=$(grep db\.url\.base /root/.xcolab.application.properties | awk -F '//' '{print $2}' | awk -F ':' '{print $2}')
+database_user=$(grep db\.user /root/.xcolab.application.properties | awk -F '=' '{print $2}')
+database_password=$(grep db\.password /root/.xcolab.application.properties | awk -F '=' '{print $2}')
 
-JAVA_OPTS="-Dserver.port=18085 -Xmx1G -Xms256M -XX:-OmitStackTraceInFastThrow -Dspring.profiles.active=docker"
+database_accessible=0
+while [[ "$database_accessible" == "0" ]]; do
+    mysqladmin -h $database_host -P $database_port -u $database_user --password=$database_password status > /dev/null 2&>1
+    if [ $? -eq 0 ]
+    then
+        database_accessible=1
+        echo "Database connection test OK"
+    else
+        echo "Database connection test NOK. Retrying."
+        sleep 3
+    fi
+done
 
-exec java -jar $JAVA_OPTS /admin-service-1.0-SNAPSHOT.jar
+JAVA_OPTS="-Dserver.port=18084 -Xmx1G -Xms256M -XX:-OmitStackTraceInFastThrow -Dspring.profiles.active=docker"
+
+exec java -jar $JAVA_OPTS /activities-service-1.0-SNAPSHOT.jar
